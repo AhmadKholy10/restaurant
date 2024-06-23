8fd702d84b4e7b05a3595b9f94e2ec18f20a5dc0
AhmedAbdelazeem98 <55737870+AhmedAbdelazeem98@users.noreply.github.com>
2024-06-24 00:56:02 +0300

    first commit

---
+++ backend/src/components/AppLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="min-h-full bg-gray-200 flex">
+    <!--    Sidebar-->
+    <Sidebar :class="{'-ml-[200px]': !sidebarOpened}"/>
+    <!--/    Sidebar-->
+
+    <div class="flex-1">
+      <Navbar @toggle-sidebar="toggleSidebar"></Navbar>
+      <!--      Content-->
+      <main class="p-6">
+        <router-view></router-view>
+      </main>
+      <!--      Content-->
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import {ref, computed, onMounted, onUnmounted} from 'vue'
+import Sidebar from "./Sidebar.vue";
+import Navbar from "./Navbar.vue";
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
